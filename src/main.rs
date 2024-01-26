52d2ec94ef9edcb93b5372159d0bf940f22609b2
bittchal <m150454055677@foxmail.com>
2024-01-26 21:26:55 +0800

    DCH:dfs查找ast代码块

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 mod clang;
 mod toolkit;
 use std::path::PathBuf;
+use std::vec;
 
+use clang::cparser::{RULE_compoundStatement, RULE_functionDefinition};
 use clap::Parser;
+
+use petgraph::{csr::NodeIndex, Graph};
+use toolkit::nodes::ASTNode;
+use petgraph::visit::Dfs;
 
 use crate::toolkit::{etc::{generate_png_by_graph, read_file_content}, gen_ast::parse_as_ast_graph};
 #[derive(Parser)]
@@ -12,6 +18,24 @@ struct Cli {
     #[arg(short, long, value_name = "FILE",default_value = "./demo.c")]
     c_file_path: PathBuf
 }
+
+///dfs遍历ast树寻找源文件代码块
+fn dfs_ast(ast_tree:&Graph<ASTNode,(), petgraph::Directed>,current_astn:petgraph::prelude::NodeIndex) -> Vec<String>{
+    let mut dfs = Dfs::new(&ast_tree, current_astn);
+    let mut vec:Vec<String> = Vec::new(); 
+    while let Some(N) = dfs.next(&ast_tree) {
+        if ast_tree[N].rule_id == RULE_functionDefinition {
+            for next in ast_tree.neighbors_directed(N, petgraph::Direction::Outgoing){
+                if ast_tree[next].rule_id == RULE_compoundStatement{
+                    vec.push(ast_tree[next].text.clone());
+                }
+            }
+        }
+    }
+    vec
+}
+
+
 fn main() {
     // 读取命令选项，诸如 -c 表示代码文件地址
     // 你也可以通过运行 cargo run -- --help 来查看所有可用选项
@@ -22,6 +46,12 @@ fn main() {
 
     // 生成 petgraph 图对应的 png 
     generate_png_by_graph(&*g.borrow(),"graph".to_string());  
+
+    //dfs遍历ast
+    let mut v = dfs_ast(&*g.borrow(),NodeIndex::from(0));
+    for node in v{
+        println!("{}",node);
+    }
 
     println!("Hello, world!");
 }
