91f69ef0a4c3f1a420adf37e20c0e73b4707533f
MilkBlock <mineral@gmail.com>
2024-01-28 18:44:42 +0800

    neightbors_ast complete

--- src/toolkit/ast_node.rs
+++ src/toolkit/ast_node.rs
@@ -1,6 +1,6 @@
-use std::fmt::Debug;
+use std::{fmt::Debug, thread::current};
 
-use petgraph::{adj::NodeIndex, graph::{self, DiGraph}, visit::IntoNeighbors, Graph};
+use petgraph::{adj::{Neighbors, NodeIndex}, graph::{self, DiGraph},  visit::IntoNeighbors, Graph};
 
 use crate::antlr_parser::cparser::ruleNames;
 use petgraph::visit::{Dfs, Walker};
@@ -23,18 +23,24 @@ impl Debug for AstNode{
 }
 
 
-pub fn dfs_ast<'a>(ast_tree:&'a AstTree,current_astn:NodeIndex,aim_index: usize) -> impl Iterator<Item = u32> + 'a{
+/// 返回 start 所有子节点以dfs序返回的特定rule_id 的迭代器
+pub fn dfs_ast<'a>(ast_tree:&'a AstTree,start:NodeIndex,target_rule_id: usize) -> impl Iterator<Item = u32> + 'a{
     // let ast_tree = &*ast_tree_rc.borrow();
-    let dfs = Dfs::new(ast_tree, NodeIndex::from(current_astn));
+    let dfs = Dfs::new(ast_tree, NodeIndex::from(start));
     let dfs_iter= dfs.iter(ast_tree);
     let dfs_iter = dfs_iter.map(|x|x.index() as u32);
     let dfs_iter = dfs_iter.filter(move |x| ->bool {
         let ast_tree_2 = &*ast_tree;
         let ast_node =ast_tree_2.node_weight(NodeIndex::from(*x));
-        ast_node.unwrap().rule_id ==  aim_index
+        ast_node.unwrap().rule_id ==  target_rule_id
     });
     dfs_iter
 }
 
+/// 返回 start 下一层特定rule_id 的迭代器
+pub fn neighbors_ast<'a>(ast_tree:&'a AstTree,start:NodeIndex,target_rule_id: usize) ->impl Iterator<Item = u32> + 'a {
+    let ns = ast_tree.neighbors(NodeIndex::from(start));
+    ns.map(|x| x.index() as u32).filter(move |x| ast_tree.node_weight(NodeIndex::from(*x)).unwrap().rule_id == target_rule_id )
+} 
 
 
