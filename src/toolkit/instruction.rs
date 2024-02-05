dbd8ffba9dc8c9f3fe43430b317e7b357b283902
sGulugulu <s1223742364@outlook.com>
2024-02-06 03:09:13 +0800

    instruction struct define

--- src/toolkit/instruction.rs
+++ src/toolkit/instruction.rs
@@ -1,6 +1,6 @@
-use super::symbol_table::SymbolIndex;
+use super::symbol_table::{Symbol, SymbolBehavior, SymbolIndex};
 
-pub enum Op{
+pub enum ArithOp{        
     Add{
         a : SymbolIndex ,
         b : SymbolIndex ,
@@ -17,35 +17,75 @@ pub enum Op{
         a : SymbolIndex ,
         b : SymbolIndex ,
     },
+    Icmp{
+        plan : IcmpPlan,
+        a : SymbolIndex ,       //寄存器或者数
+        b : SymbolIndex ,
+    },
+}
+pub struct FuncOp{
+    func:SymbolIndex,
+    args:Vec<SymbolIndex>       //存储所有的实参
+}
+pub struct PhiPair{
+    value : SymbolIndex,
+    bb : SymbolIndex,
+}
+pub struct PhiOp{
+    syms : Vec<PhiPair>,
+}
+pub enum MemOp{
+    Load{
+        ptr: SymbolIndex,
+    },
+    Store{
+        value : SymbolIndex,
+        ptr: SymbolIndex,
+    },
+    Alloca{
+        align:u32,
+    }
+}
+pub enum Op{
+    ArithOp(ArithOp),
+    FuncOp(FuncOp),
+    Jump(JumpOp)
+}
+enum Instruction{
     Assign{
-        a : SymbolIndex ,
-        b : Box<Op> ,
+        lhs:SymbolIndex,
+        rhs:ArithOp,
     },
-    Jump{
-        des_label : u32 , // 这是 cfg blcok 的 索引
-    },
+    NotAssign{
+        op:Op,
+    }
+}
+enum IcmpPlan{
+    eq,ne,              // 等与不等
+    ugt,uge,ult,ule,    //无符号比较
+    sgt,sge,slt,sle,    //有符号比较
+}
+struct ComparedPair{
+    compared : SymbolIndex,
+    label : SymbolIndex,
+}
+enum JumpOp{
     Ret{
         ret_sym : SymbolIndex , // 这是返回的类型
     },
     Br{
-        t1 : u32 ,
-        t2 : u32 ,
+        cond : SymbolIndex ,
+        t1 : SymbolIndex ,
+        t2 : SymbolIndex,
     },
-    Phi{
-        syms : Vec<SymbolIndex>
+    Switch{
+        cond : SymbolIndex,
+        default : SymbolIndex,
+        compared: Vec<ComparedPair>,
     },
-    Load{
-        des: SymbolIndex, 
-        src: SymbolIndex,
+    Jump{
+        cfg_des_label : u32 , // 这是 cfg blcok 的 索引
     },
-    Store{
-        des: SymbolIndex, 
-        src: SymbolIndex,
-    }
 }
 
-struct Instruction{
-    
-
-}
 
