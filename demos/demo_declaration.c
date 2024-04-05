2abe596c333447c8ff2d48fedc499808f56abc5f
bittchal <m150454055677@foxmail.com>
2024-04-05 22:48:04 +0800

    提交新demo

--- demos/demo_declaration.c
+++ demos/demo_declaration.c
@@ -1,21 +1,33 @@
-int main(){
-    int x;
-    int q = 2,k;
-    int a,b,c,d,e;
-    a = 2+3;
-    b = 2-3*4/2+2*7;
-    c = a+b;
-    d = a*3;
-    d += 1;
-    d -= 2;
-    d *= a;
-    d /= b+3;
-    b++;
-    b--;
-    ++c;
-    --c;
-    e = ++a+b++;
+int main() {
+    // 调用辅助函数
+    int finalResult = performOperations(15, 3);
+    // 注意：为了满足要求，我们不在main函数中输出结果
+    return 0;
 }
-void fun(int a,int b){
-    a = b+a;
+
+int performOperations(int a, int b) {
+    // 逻辑运算
+    bool logicResult = (a >= b) || (b <= a) && !(a == 0);
+
+    // 比较运算
+    bool comparisonResult = (a % 2 == 0) == (b % 2 != 0);
+
+    // 类型转换
+    double division = 1+2;
+    int d = 1+1.3;
+    double e = 1*3.23-3;
+    
+    // 按位运算
+    int bitwiseAndResult = a & b;
+    int bitwiseOrResult = a | b;
+
+    // 移位运算
+    int leftShiftResult = a << 1;
+    int rightShiftResult = b >> 1;
+
+    // 不使用变量的直接赋值结果进行操作以避免分支语句
+    // 计算一个综合得分，简化为将所有操作结果相加
+    int score = 1*3;
+
+    return score;
 }
