caf6d83f790c212b8e203f221fa4de5a93fe5a2f
bittchal <m150454055677@foxmail.com>
2024-07-26 16:34:22 +0800

    1

--- src/toolkit/loop_unrolling.rs
+++ src/toolkit/loop_unrolling.rs
@@ -4,7 +4,7 @@ use super::cfg_node::{CfgGraph, CfgNodeType};
 
 ///判断是否为whileloop，然后提取条件判断变量
 pub fn get_while_condition_symidx(cfg_node:u32,cfg_graph:CfgGraph){
-    if let CfgNodeType::WhileLoop { ast_expr_node } = node!(at cfg_node in cfg_graph){
+    // if let CfgNodeType::WhileLoop { ast_expr_node } = node!(at cfg_node in cfg_graph){
         
-    }else{}
+    // }else{}
 }
